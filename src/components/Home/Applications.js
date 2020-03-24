2e9229dfcf8dc6ae95b00f2b8acdf4ca6a1b54a4
Cláudio Henriques <c.henriques@ua.pt>
2020-03-24 11:51:34 +0000

    Update applications

--- src/components/Home/Applications.js
+++ src/components/Home/Applications.js
@@ -8,6 +8,18 @@ class Applications extends Component {
     };
   }
 
+  titleCase(str) {
+    str = str.split('_').join(' ')
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+      // You do not need to check if i is larger than splitStr length, as your for does that for you
+      // Assign it back to the array
+      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    // Directly return the joined string
+    return splitStr.join(' ');
+  }
+
   getApplicationsList() {
     var applications_list = document.getElementById('applications_list');
     var card_deck_issue = document.createElement('div');
@@ -20,46 +32,112 @@ class Applications extends Component {
         header.setAttribute('class', 'card-header');
         var title = document.createElement('h5');
         title.setAttribute('class', 'my-0 font-weight-normal');
-        var text = document.createTextNode(application.name);
+        var text = document.createTextNode(this.titleCase(application.name));
         title.append(text);
         header.append(title);
         var body = document.createElement('div');
         body.setAttribute('class', 'card-body');
+
+        if (application.minecode_settings !== undefined) {
+          if (application.minecode_settings.image !== null) {
+            var img = document.createElement('img')
+            img.setAttribute('width', 96)
+            img.setAttribute('height', 96)
+            img.setAttribute('src', 'https://raw.githubusercontent.com/minecode/' + application.name + '/master/' + application.minecode_settings.image)
+            body.append(img)
+          }
+        }
+
         var description = document.createElement('div');
         description.setAttribute('class', 'mt-3 mb-3');
         text = document.createTextNode(application.description);
         description.append(text);
         body.append(description);
-        var status = document.createElement('div');
-        status.setAttribute('class', 'mt-3 mb-4');
-        var a = document.createElement('a')
-        a.setAttribute('target', '_blank')
-        a.setAttribute('rel', 'noopener noreferrer')
-        a.setAttribute('href', '/privacy_policy.html')
-        var b = document.createElement('b')
-        var textPP = document.createTextNode('Privacy policy')
-        b.append(textPP)
-        a.append(b)
-        status.append(a);
-        body.append(status);
-        var labels = document.createElement('div');
-        labels.setAttribute('class', 'text-center');
-        var lab = document.createElement('a');
-        lab.setAttribute('class', 'mt-3 mb-4 btn btn-sm btn-outline-primary');
-        lab.setAttribute('href', application.homepage)
-        lab.setAttribute('target', '_blank')
-        lab.setAttribute('rel', 'nooper noreferrer')
-        lab.setAttribute('type', 'button')
-        var img = document.createElement('img')
-        img.setAttribute('src', 'images/google-play-store.svg')
-        img.setAttribute('alt', 'PlayStore')
-        img.setAttribute('width', '24')
-        img.setAttribute('height', '24')
-        lab.append(img)
-        var text = document.createTextNode('Get app');
-        lab.append(text);
-        labels.append(lab);
-        body.append(labels);
+        var homepageAdded = false
+        if (application.homepage !== null) {
+          var labels = document.createElement('div');
+          labels.setAttribute('class', 'text-center');
+          var lab = document.createElement('a');
+          lab.setAttribute('class', 'mt-3 mb-4 btn btn-sm btn-outline-primary');
+          lab.setAttribute('href', application.homepage)
+          lab.setAttribute('target', '_blank')
+          lab.setAttribute('rel', 'nooper noreferrer')
+          lab.setAttribute('type', 'button')
+          img = document.createElement('img')
+          img.setAttribute('src', 'images/web-page.svg')
+          img.setAttribute('alt', 'Webpage')
+          img.setAttribute('width', '24')
+          img.setAttribute('height', '24')
+          lab.append(img)
+          text = document.createTextNode(' Go to website');
+          lab.append(text);
+          labels.append(lab);
+          body.append(labels);
+          homepageAdded = true
+        }
+        if (application.minecode_settings !== undefined) {
+          if (application.minecode_settings.privacy_policy !== null) {
+            var status = document.createElement('div');
+            status.setAttribute('class', 'mt-3 mb-4');
+            var a = document.createElement('a')
+            a.setAttribute('target', '_blank')
+            a.setAttribute('rel', 'noopener noreferrer')
+            a.setAttribute('href', 'https://raw.githubusercontent.com/minecode/' + application.name + '/master/' + application.minecode_settings.privacy_policy)
+            var b = document.createElement('b')
+            var textPP = document.createTextNode('Privacy policy')
+            b.append(textPP)
+            a.append(b)
+            status.append(a);
+            body.append(status);
+          }
+          if (application.minecode_settings.link_mobile !== null) {
+            labels = document.createElement('div');
+            labels.setAttribute('class', 'text-center');
+            lab = document.createElement('a');
+            lab.setAttribute('class', 'mt-3 mb-4 btn btn-sm btn-outline-primary');
+            if (application.minecode_settings.link_mobile.includes('https://') || application.minecode_settings.link_mobile.includes('http://'))
+              lab.setAttribute('href', application.minecode_settings.link_mobile)
+            else
+              lab.setAttribute('href', 'https://' + application.minecode_settings.link_mobile)
+            lab.setAttribute('target', '_blank')
+            lab.setAttribute('rel', 'nooper noreferrer')
+            lab.setAttribute('type', 'button')
+            img = document.createElement('img')
+            img.setAttribute('src', 'images/google-play-store.svg')
+            img.setAttribute('alt', 'PlayStore')
+            img.setAttribute('width', '24')
+            img.setAttribute('height', '24')
+            lab.append(img)
+            text = document.createTextNode(' Get app');
+            lab.append(text);
+            labels.append(lab);
+            body.append(labels);
+          }
+          if (application.minecode_settings.link_website !== null && homepageAdded === false) {
+            labels = document.createElement('div');
+            labels.setAttribute('class', 'text-center');
+            lab = document.createElement('a');
+            lab.setAttribute('class', 'mt-3 mb-4 btn btn-sm btn-outline-primary');
+            if (application.minecode_settings.link_website.includes('https://') || application.minecode_settings.link_website.includes('http://'))
+              lab.setAttribute('href', application.minecode_settings.link_website)
+            else 
+              lab.setAttribute('href', 'https://' + application.minecode_settings.link_website)
+            lab.setAttribute('target', '_blank')
+            lab.setAttribute('rel', 'nooper noreferrer')
+            lab.setAttribute('type', 'button')
+            img = document.createElement('img')
+            img.setAttribute('src', 'images/web-page.svg')
+            img.setAttribute('alt', 'Webpage')
+            img.setAttribute('width', '24')
+            img.setAttribute('height', '24')
+            lab.append(img)
+            text = document.createTextNode(' Go to website');
+            lab.append(text);
+            labels.append(lab);
+            body.append(labels);
+          }
+
+        }
         card.append(header);
         card.append(body);
         card_deck_issue.append(card)
@@ -79,18 +157,28 @@ class Applications extends Component {
     .then(res => res.json())
     .then(data => {
       var count = 0
-      header.set('accept', 'application/vnd.github.mercy-preview+json')
+      header.set('Accept', 'application/vnd.github.mercy-preview+json')
       data.forEach(repo => {
-        fetch('https://api.github.com/repos/minecode/' + repo.name + '/topics', {
-          method: 'GET',
-          headers: header
-        })
-        .then(res => res.json())
-        .then(data2 => {
+        Promise.all([
+          fetch('https://api.github.com/repos/minecode/' + repo.name + '/topics', {
+            method: 'GET',
+            headers: header
+          }),
+          fetch('https://api.github.com/repos/minecode/' + repo.name + '/contents/minecode_settings.json?ref=master', {
+            method: 'GET',
+            headers: header,
+          })
+        ])
+        .then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
+        .then(([data2, data3]) => {
+          if (data3.content !== undefined) {
+            data3.content = JSON.parse(atob(data3.content))
+          }
           var temp = 0
           data.forEach(element => {
             if (element.name === repo.name) {
-              data[count].topic = data2
+              data[temp].topic = data2
+              data[temp].minecode_settings = data3.content
             }
             temp++
           })

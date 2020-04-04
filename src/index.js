4c806551dc5bbeaa2c6a8f90d96934e0647d031a
Fábio Henriques <fabiomchenriques@ua.pt>
2020-04-04 15:03:46 +0100

    init v2.0

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

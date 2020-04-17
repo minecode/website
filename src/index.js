34b0e3992e267af0f698c69b7449c8cbf30bc6dd
cfchenr <c.henriques@ua.pt>
2020-04-17 11:44:04 +0100

    remove comment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 import './index.css';
 

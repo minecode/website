814c1ce84528dcdf4fcff896998f93002b36e15c
cfchenr <c.henriques@ua.pt>
2020-04-20 23:26:37 +0100

    Updates

---
+++ precache-manifest.67bd78025623dac8958233ff302d6789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe779ff1c203be02f4c3966161f54f65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df42749eb650164cd7d9",
+    "url": "/static/css/main.813f1fe2.chunk.css"
+  },
+  {
+    "revision": "5144345001bfa362da68",
+    "url": "/static/js/2.f3888d5a.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.f3888d5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df42749eb650164cd7d9",
+    "url": "/static/js/main.c3e129b0.chunk.js"
+  },
+  {
+    "revision": "2b16e05acbb0eb80e55c",
+    "url": "/static/js/runtime-main.2b2cda15.js"
+  }
+]);

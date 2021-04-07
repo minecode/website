9bfd0b5d5ab528414789950e227d38522b233b5e
Cláudio Henriques <c.henriques@ua.pt>
2021-04-07 12:07:29 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,3 +13,10 @@ module.exports = {
 		REACT_APP_APIKEY: '97b2a912e67b0ae98cd5:e644319491835bcaa1dd08693df8185e6c950e6a',
 	}
 };
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/minecode/minecode.github.io/master/' : '',
+}

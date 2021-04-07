0c34d5d04058df971538eb8dc27e55056a3e13d1
minecodebot <62449777+minecodebot@users.noreply.github.com>
2021-04-07 17:48:02 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,5 +13,5 @@ module.exports = {
 	env: {
 		REACT_APP_APIKEY: '97b2a912e67b0ae98cd5:e644319491835bcaa1dd08693df8185e6c950e6a',
 	},
-  assetPrefix: '/minecode.github.io/'
+  assetPrefix: 'https://minecode.github.io/'
 }

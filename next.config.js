0ec0f430ffd73eb30777d79874587954d64249cf
minecodebot <62449777+minecodebot@users.noreply.github.com>
2021-04-07 17:23:33 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -18,5 +18,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/minecode/minecode.github.io/master/' : '',
+  assetPrefix: isProd ? 'https://minecode.github.io' : '',
 }
